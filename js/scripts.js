9dd982a8a66d70181c14faffb8c82bcde3933345
caleb <ck.caleb17@gmail.com>
2019-02-16 17:21:18 +0300

    add paragraph block elements to the page

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function findout() {
+var inp1=document.getElementById("data1").value;
+var inp2=document.getElementById("data2").value;
+var inp3=document.getElementById("data3").value;
+var sideA=parseInt(inp1);
+var sideB=parseInt(inp2);
+var sideC=parseInt(inp3);
+if (sideA === sideB && sideB === sideC && sideC === sideA){
+  alert("EQUILATERAL TRAIANGLE");
+}
+else if (sideA === sideB && sideA !== sideC) {
+  alert("ISOSCELES TRIANGLE");
+}else if (sideA === sideC && sideA !== sideB) {
+  alert("ISOSCELES TRAIANGLE");
+}else if (sideB === sideC && sideB !== sideA) {
+  alert("ISOSCELES  TRAIANGLE");
+}else if (sideA !== sideB && sideB !== sideC && sideA !== sideC && sideA + sideB > sideC && sideB + sideC > sideA){
+  alert("SCALENE TRIANGLE");
+}else if (sideA + sideB <= sideC) {
+  alert ("NOT A TRIANGLE");
+}else if (sideA + sideC <=sideB) {
+  alert ("NOT A TRIANGLE");
+}else if (sideB + sideC <=sideA) {
+  alert ("NOT A TRIANGLE");
+}
+}
